47d2020b45ff2221ee21a21fcda08dad8cd75415
Kamie Short <kamie.short@gmail.com>
2022-06-14 08:17:01 -0700

    getAll book model set up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,25 +15,21 @@ describe('test routes for books and authors', () => {
         id: 1,
         title: 'Hiking the Pacific Crest Trail',
         released: 2016,
-        author: 'Eli Boschetto',
       },
       {
         id: 2,
         title: 'The Joyful Vegan',
         released: 2019,
-        author: 'Collen Patrick-Goudreau',
       },
       {
         id: 3,
         title: 'The Bullet Journal Method',
         released: 2018,
-        author: 'Ryder Carrol',
       },
       {
         id: 4,
         title: 'Like a Mother',
         released: 2020,
-        author: 'Angela Garbes',
       },
     ]);
   });
