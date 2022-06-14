ab4334f2240e21535a60369afe44b9ea1d28cb93
Kamie Short <kamie.short@gmail.com>
2022-06-14 11:49:47 -0700

    set up joined table for authors and books

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,21 +3,28 @@
 
 Drop table if exists books;
 Drop table if exists authors;
+DROP table if exists books_authors;
 
 CREATE table books (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR NOT NULL,
     released INT NOT NULL
 );
 
 CREATE table authors (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     dob VARCHAR NOT NULL,
     pob VARCHAR NOT NULL
 );
 
-
+CREATE table books_authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    book_id BIGINT,
+    author_id BIGINT,
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id)
+);
 
 
 INSERT INTO books (title, released) VALUES
@@ -31,4 +38,11 @@ INSERT INTO authors (name, dob, pob) VALUES
 ('Collen Patrick-Goudreau', '6-JUN-1977', 'San Francisco, California'),
 ('Ryder Carrol', '13-MAR-1978', 'Portland, Maine'),
 ('Angela Garbes', '24-JUL-1983', 'Seattle, Washington');
+
+INSERT INTO books_authors (book_id, author_id) VALUES 
+(1,1),
+(2,2),
+(3,3),
+(4,4);
+
 
